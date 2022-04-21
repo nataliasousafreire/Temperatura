f01953806b1ebf7c356b849963e08562cb0837ec
nataliasousafreire <nsf@icomp.ufam.edu.br>
2022-04-20 21:47:46 -0400

    adicioando projeto

---
+++ src/main/java/br/com/violencia/conversor/Main.java
@@ -0,0 +1,9 @@
+package br.com.violencia.conversor;
+
+public class Main {
+    public static void main(String[] args){
+        Conversor c = new Conversor('K', 300);
+
+        c.converter('F');
+    }
+}
